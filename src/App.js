8fe85c88b2d62575b6470d95c753b4f309c12e29
Gerardo Gallegos <gallegosg13@gmail.com>
2019-07-07 17:32:36 -0700

    Add Nav bar and official start of page

--- src/App.js
+++ src/App.js
@@ -2,12 +2,48 @@ import React, { Component } from 'react';
 import './App.css';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      page: (<div><h1>Gerardo Gallegos</h1>
+      <p>Making cool things, eventually</p></div>)
+    };
+  }
+  
+  handleClick = (e) => {
+    const {id} = e.target
+    let page = '';
+    switch(id){
+      case 'home':
+        page = (<div><h1>Gerardo Gallegos</h1>
+          <p>Making cool things, eventually</p></div>)
+        break;
+      case 'contact':
+        page = (
+          <div>call me at ### but dont call me late for dinner</div>
+        )
+        break;
+      case 'about':
+        page = (<div>
+          <p>Full time programmer, part time racecar driver (in my mind)</p></div>)
+        break;
+      default: 
+        break;
+    }
+    this.setState({page})
+  }
   render() {
     return (
       <div className='App'>
+        <div className="topnav">
+          <p onClick={this.handleClick} id={'home'} className="active" >Home</p>
+          <p onClick={this.handleClick} id={'contact'}>Contact</p>
+          <p onClick={this.handleClick} id={'about'}>About</p>
+        </div>
         <div className='center'>
-          <h1>Gerardo Gallegos</h1>
-          <p>Making cool things, eventually</p>
+          {/* <h1>Gerardo Gallegos</h1>
+          <p>Making cool things, eventually</p> */}
+          {this.state.page}
         </div>
       </div>
     );
